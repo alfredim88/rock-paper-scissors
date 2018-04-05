9b3187f6ddd31731c28391992baa6e47d3fa1759
Alfredo Faria <alfredo.faria88@gmail.com>
2018-04-05 02:47:22 +0000

    functional rock paper scissors html

--- script/main.js
+++ script/main.js
@@ -3,38 +3,76 @@ var playerScore = 0;
 var pcScore = 0;
 var plays = 0;
 var pcSelection = computerPlay();
+var playerSelection = "";
 
 function computerPlay (){
   switch(Math.floor(Math.random()*3)+1){
     case 1:
+    document.getElementById("pcWeaponImage").src = "./images/paper.png";
     return "paper";
     case 2:
+    document.getElementById("pcWeaponImage").src = "./images/scissors.png";
     return "scissors";
     case 3:
+    document.getElementById("pcWeaponImage").src = "./images/rock.png";
     return "rock";
     }
 
 }
 
-var playerSelection = prompt("Choose between Rock, Paper and Scissors:").toLowerCase();
-
 function playRound(pcSelection, playerSelection) {
   plays += 1;
+  pcSelection = computerPlay();
 
   if (pcSelection === playerSelection){
-  return console.log("player chose " + playerSelection + " and computer chose "+ pcSelection + ", it's a DRAW!");
+  return resultPlaceholder.textContent = "DRAW!";
 }
   if (playerSelection === "paper" && pcSelection === "rock") {
     playerScore += 1;
-     return console.log("player chose " + playerSelection + " and computer chose "+ pcSelection + ". Player WINS!");
+
+    document.getElementById("playerScorePlaceholder").innerHTML = playerScore;
+    resultPlaceholder.textContent = "PLAYER WINS!";
+    return ;
         }
+
+  if (pcSelection === "paper" && playerSelection === "rock") {
+      pcScore += 1;
+
+      document.getElementById("computerScorePlaceholder").innerHTML = pcScore;
+      resultPlaceholder.textContent = "COMPUTER WINS!";
+      return ;
+            }
 
     if (possibleResults[playerSelection]>possibleResults[pcSelection]){
       playerScore += 1;
-      return console.log("player chose " + playerSelection + " and computer chose "+ pcSelection + ". Player Wins!");
+
+      document.getElementById("playerScorePlaceholder").innerHTML = playerScore;
+      resultPlaceholder.textContent = "PLAYER WINS!";
+      return;
     }
 
- return console.log("player chose " + playerSelection + " and computer chose "+ pcSelection + ". Computer Wins!");
+    pcScore += 1;
+
+    document.getElementById("computerScorePlaceholder").innerHTML = pcScore;
+    resultPlaceholder.textContent = "COMPUTER WINS!";
+
+ return;
 }
 
-playRound(pcSelection, playerSelection);
+function playerWeaponRock(){;
+  document.getElementById("playerWeaponImage").src = "./images/rock.png";
+  playerSelection = "rock";
+  playRound(pcSelection, playerSelection);
+};
+
+function playerWeaponPaper(){
+  document.getElementById("playerWeaponImage").src = "./images/paper.png";
+  playerSelection = "paper";
+  playRound(pcSelection, playerSelection);
+};
+
+function playerWeaponScissors(){
+  document.getElementById("playerWeaponImage").src = "./images/scissors.png";
+  playerSelection = "scissors";
+  playRound(pcSelection, playerSelection);
+};
